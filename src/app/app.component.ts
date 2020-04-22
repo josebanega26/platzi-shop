71fc323fb3033017d9b4695713277c3dcba94f65
Jose Banega <banegajose26@gmail.com>
2020-04-22 19:36:24 -0300

    Add products-list component and connect it via Input/Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit  } from '@angular/core';
+import {Product} from './products/products.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  // @Input
   title = 'platzi-shop';
+
+  addCart(index: number){
+    console.log('you click the children', index)
+  }
+  ngOnInit(){
+  }
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
 }
